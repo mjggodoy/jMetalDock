7364409b11d15d56f2bd9bfc412fdeecf419b867
Esteban López Camacho <esteban@lcc.uma.es>
2016-11-15 14:20:53 +0100

    Thread executes autodock command.

--- src/main/java/es/uma/khaos/docking_service/threadpool/WorkerThread.java
+++ src/main/java/es/uma/khaos/docking_service/threadpool/WorkerThread.java
@@ -1,8 +1,15 @@
 package es.uma.khaos.docking_service.threadpool;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import es.uma.khaos.docking_service.service.DatabaseService;
 
 public class WorkerThread implements Runnable {
+	
+	private final String AUTODOCK_LOCATION = "/opt/autodock/";
+	private final String AUTODOCK_EXECUTABLE = "autodock4";
 
 	private String command;
 	private int id;
@@ -28,11 +35,42 @@ public class WorkerThread implements Runnable {
 	}
 	
 	private void processCommand() {
+		
+		String command = AUTODOCK_LOCATION + AUTODOCK_EXECUTABLE;
+		String s = null;
+		
 		try {
-			Thread.sleep(50000);
+		
+			// Ejcutamos el comando
+	        Process p = Runtime.getRuntime().exec(command);
+	
+	        BufferedReader stdInput = new BufferedReader(new InputStreamReader(
+	                p.getInputStream()));
+	
+	        BufferedReader stdError = new BufferedReader(new InputStreamReader(
+	                p.getErrorStream()));
+	
+	        // Leemos la salida del comando
+	        System.out.println("Esta es la salida standard del comando:\n");
+	        while ((s = stdInput.readLine()) != null) {
+	            System.out.println(s);
+	        }
+	
+	        // Leemos los errores si los hubiera
+	        System.out
+	                .println("Esta es la salida standard de error del comando (si la hay):\n");
+	        while ((s = stdError.readLine()) != null) {
+	            System.out.println(s);
+	        }
+	        
+	        Thread.sleep(50000);
+        
 		} catch (InterruptedException e) {
 			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		
 	}
 	
 	@Override
