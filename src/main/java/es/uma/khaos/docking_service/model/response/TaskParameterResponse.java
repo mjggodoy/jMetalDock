49eef8c2a735b4366cabd9b77e6a2e85409d4d60
María Jesús García Godoy <mjggodoy@gmail.com>
2016-12-07 11:58:18 +0100

    Json object configured

--- src/main/java/es/uma/khaos/docking_service/model/response/TaskParameterResponse.java
+++ src/main/java/es/uma/khaos/docking_service/model/response/TaskParameterResponse.java
@@ -1,5 +1,44 @@
 package es.uma.khaos.docking_service.model.response;
 
-public class TaskParameterResponse {
+import java.util.ArrayList;
 
+import es.uma.khaos.docking_service.model.Parameter;
+
+public class TaskParameterResponse {
+	
+	private int id;
+	private String token;
+	private String state;
+	private ArrayList<Parameter> parameter;
+	
+
+	public TaskParameterResponse(int id, String token, String state,
+			ArrayList<Parameter> parameter) {
+		super();
+		this.id = id;
+		this.token = token;
+		this.state = state;
+		this.parameter = parameter;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public String getToken() {
+		return token;
+	}
+
+
+	public String getState() {
+		return state;
+	}
+
+
+	public ArrayList<Parameter> getParameter() {
+		return parameter;
+	}
+	
 }
