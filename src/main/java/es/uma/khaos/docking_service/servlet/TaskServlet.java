79a418bc5f753c2387695b94e76b3ae8bb216f3f
María Jesús García Godoy <mjggodoy@gmail.com>
2016-12-21 14:43:39 +0100

    Controlling max and min of values and also when parameters are not included

--- src/main/java/es/uma/khaos/docking_service/servlet/TaskServlet.java
+++ src/main/java/es/uma/khaos/docking_service/servlet/TaskServlet.java
@@ -5,6 +5,8 @@ import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import javax.servlet.ServletException;
@@ -108,6 +110,73 @@ public class TaskServlet extends HttpServlet {
 		ThreadPoolService.getInstance().execute(worker);
 		
 	}
+	
+	/*if(popSize > Constants.DEFAULT_MAX_NUMBER_POPULATION_SIZE){
+		
+		popSize = Constants.DEFAULT_MAX_NUMBER_POPULATION_SIZE;
+		
+	}else if (popSize < Constants.DEFAULT_MIN_NUMBER_POPULATION_SIZE){
+		
+		popSize = Constants.DEFAULT_MIN_NUMBER_POPULATION_SIZE;		
+	}*/
+	
+	/*if(runs > Constants.DEFAULT_MAX_NUMBER_RUNS){
+		
+		runs = Constants.DEFAULT_MAX_NUMBER_RUNS;
+		
+	}else if(runs < Constants.DEFAULT_MIN_NUMBER_RUNS){
+		
+		runs = Constants.DEFAULT_MIN_NUMBER_RUNS;
+
+	}*/
+	
+	protected int maxMinRangeEvals(int evals){
+	
+		
+		if(evals > Constants.DEFAULT_MAX_NUMBER_EVALUATIONS){
+			
+			evals = Constants.DEFAULT_MAX_NUMBER_EVALUATIONS;
+	
+		}else if(evals < Constants.DEFAULT_MIN_NUMBER_EVALUATIONS){
+			
+			evals = Constants.DEFAULT_MIN_NUMBER_EVALUATIONS;		
+		}
+	
+		return evals;
+	}
+	
+	
+	
+	protected int maxMinRangePopSize(int popSize){
+		
+		if(popSize > Constants.DEFAULT_MAX_NUMBER_POPULATION_SIZE){
+			
+			popSize = Constants.DEFAULT_MAX_NUMBER_POPULATION_SIZE;
+			
+		}else if(popSize < Constants.DEFAULT_MIN_NUMBER_POPULATION_SIZE){
+			
+			popSize = Constants.DEFAULT_MIN_NUMBER_POPULATION_SIZE;
+		}
+		
+		return popSize;
+	}
+	
+	
+	protected int maxMinRangeRuns(int runs){
+		
+		if(runs > Constants.DEFAULT_MAX_NUMBER_RUNS){
+			
+			runs = Constants.DEFAULT_MAX_NUMBER_RUNS;
+			
+		}else if (runs < Constants.DEFAULT_MIN_NUMBER_RUNS){
+			
+			runs = Constants.DEFAULT_MIN_NUMBER_RUNS;		
+		}
+		
+		return runs;
+		
+	}
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
@@ -116,6 +185,8 @@ public class TaskServlet extends HttpServlet {
 		
 		//TODO: Check correctness of parameters
 		
+		ArrayList<Integer> parameters = new ArrayList<Integer>();
+
 		String runsParam  = request.getParameter("runs");
 		String algorithm  = request.getParameter("algorithm");
 		String evalsParam = request.getParameter("evaluations");
@@ -133,70 +204,63 @@ public class TaskServlet extends HttpServlet {
 						HttpServletResponse.SC_BAD_REQUEST,
 						String.format(Constants.RESPONSE_MANDATORY_PARAMETER_ERROR, "algorithm"));
 				
-			}else if(StringUtils.isNullOrEmpty(runsParam)){
+			}else{
 				
-				int popSize = Integer.parseInt(popSizeParam);
-				int evals = Integer.parseInt(evalsParam);
-				int runs = Constants.DEFAULT_NUMBER_RUNS;
-				System.out.println("runs" + runs);
-				defaultValues(popSize, evals, runs, algorithm, objectiveOpt, response);
+				if(StringUtils.isNullOrEmpty(runsParam)){
 				
-			}else if(StringUtils.isNullOrEmpty(evalsParam)){
-				
-				int popSize = Integer.parseInt(popSizeParam);
-				int runs = Integer.parseInt(runsParam);
-				int evals = Constants.DEFAULT_NUMBER_EVALUATIONS;
-				System.out.println("evals" + evals);
-				defaultValues(popSize, evals, runs, algorithm, objectiveOpt, response);
-			
-			}else if(StringUtils.isNullOrEmpty(popSizeParam)){
-				
-				int evals = Integer.parseInt(evalsParam);
-				int runs = Integer.parseInt(runsParam);
-				int popSize = Constants.DEFAULT_NUMBER_POPULATION_SIZE;
-				System.out.println("popSize" + popSize);
-				defaultValues(popSize, evals, runs, algorithm, objectiveOpt, response);
+					int popSize = Integer.parseInt(popSizeParam);
+					int evals = Integer.parseInt(evalsParam);
+					int runs = Constants.DEFAULT_NUMBER_RUNS;
+					System.out.println("runs" + runs);
+					popSize = maxMinRangePopSize(popSize);
+					evals = maxMinRangeEvals(evals);
+					defaultValues(popSize, evals, runs, algorithm, objectiveOpt, response);
 					
-			}else{	
+				}
 				
-				int runs = Integer.parseInt(runsParam);
-				int evals = Integer.parseInt(evalsParam);
-				int popSize = Integer.parseInt(popSizeParam);
+				if(StringUtils.isNullOrEmpty(evalsParam)){
 				
-				if((runs>=Constants.DEFAULT_MIN_NUMBER_RUNS && runs<= Constants.DEFAULT_MAX_NUMBER_RUNS) && (evals>=Constants.DEFAULT_MIN_NUMBER_EVALUATIONS && evals<=Constants.DEFAULT_MAX_NUMBER_EVALUATIONS)
-						&& (popSize>=Constants.DEFAULT_MIN_NUMBER_POPULATION_SIZE && popSize<=Constants.DEFAULT_MAX_NUMBER_POPULATION_SIZE)){
-			
-					Random sr = SecureRandom.getInstance("SHA1PRNG");
-					String token = new BigInteger(130, sr).toString(32);
-			
-					Task task = DatabaseService.getInstance().insertTask(token);
-					ParameterSet parameters = DatabaseService.getInstance().insertParameter(algorithm, evals, popSize, runs, objectiveOpt, task.getId());
-			
-					Gson gson = new Gson();
-					task.setParameters(parameters);
-					String json = gson.toJson(task);
-			
-					response.setStatus(HttpServletResponse.SC_CREATED);
-					PrintWriter out = response.getWriter();
-					out.print(json);
-					out.flush();
-					Runnable worker = new WorkerThread("DOCKING", task.getId(), algorithm, runs, popSize, evals, objectiveOpt);
-					ThreadPoolService.getInstance().execute(worker);
-			
+					int popSize = Integer.parseInt(popSizeParam);
+					int runs = Integer.parseInt(runsParam);
+					int evals = Constants.DEFAULT_NUMBER_EVALUATIONS;
+					System.out.println("evals" + evals);
+					popSize = maxMinRangePopSize(popSize);
+					runs = maxMinRangeRuns(runs);
+					defaultValues(popSize, evals, runs, algorithm, objectiveOpt, response);
+					
+				}
 				
-				}else{
+				if(StringUtils.isNullOrEmpty(popSizeParam)){
 				
-					System.out.println("error m’nimos");
-					response.sendError(
-							HttpServletResponse.SC_BAD_REQUEST,
-							String.format(Constants.RESPONSE_MIN_MAX_VALUES, "algorithm"));		
+					int evals = Integer.parseInt(evalsParam);
+					int runs = Integer.parseInt(runsParam);
+					int popSize = Constants.DEFAULT_NUMBER_POPULATION_SIZE;
+					System.out.println("popSize" + popSize);
+					evals = maxMinRangeEvals(evals);
+					runs = maxMinRangeRuns(runs);
+					defaultValues(popSize, evals, runs, algorithm, objectiveOpt, response);
+				
+				}
+				
+				if(!StringUtils.isNullOrEmpty(runsParam) && !StringUtils.isNullOrEmpty(evalsParam)
+						&& !StringUtils.isNullOrEmpty(popSizeParam)){
+					
+					int runs = Integer.parseInt(runsParam);
+					int evals = Integer.parseInt(evalsParam);
+					int popSize = Integer.parseInt(popSizeParam);
+					popSize = maxMinRangePopSize(popSize);
+					evals = maxMinRangeEvals(evals);
+					runs = maxMinRangeRuns(runs);
+					defaultValues(popSize, evals, runs, algorithm, objectiveOpt, response);	
 				}
 			}
+				
 				
 		} catch (Exception e) {
 			e.printStackTrace();
 			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, Constants.RESPONSE_ERROR_DATABASE);
 		}
+		
 		
 	}
 }
