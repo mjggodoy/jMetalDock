616c4aff56467c0dc59014adfa677d9cddb5bef1
María Jesús García Godoy <mjggodoy@gmail.com>
2017-02-07 14:30:19 +0100

    Setters fixed!

--- src/main/java/es/uma/khaos/docking_service/model/ParameterSet.java
+++ src/main/java/es/uma/khaos/docking_service/model/ParameterSet.java
@@ -1,7 +1,5 @@
 package es.uma.khaos.docking_service.model;
 
-import javax.xml.bind.annotation.XmlRootElement;
-
 public class ParameterSet {
 
 	private int id;
@@ -11,6 +9,8 @@ public class ParameterSet {
 	private int run;
 	private int objective;
 	private int task_id;
+	
+	public ParameterSet(){}
 	
 	public ParameterSet(int id, String algorithm, int evaluations, int populationSize, int runs,
 			int objectives, int task_id) {
@@ -51,5 +51,32 @@ public class ParameterSet {
 	public int getTask_id() {
 		return task_id;
 	}
-	
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setAlgorithm(String algorithm) {
+		this.algorithm = algorithm;
+	}
+
+	public void setEvaluation(int evaluation) {
+		this.evaluation = evaluation;
+	}
+
+	public void setPopulationSize(int populationSize) {
+		this.populationSize = populationSize;
+	}
+
+	public void setRun(int run) {
+		this.run = run;
+	}
+
+	public void setObjective(int objective) {
+		this.objective = objective;
+	}
+
+	public void setTask_id(int task_id) {
+		this.task_id = task_id;
+	}
 }
