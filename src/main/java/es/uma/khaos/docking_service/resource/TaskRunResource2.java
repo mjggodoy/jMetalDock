d0e1d3ea1ea9726ab248004cf3c265ec1c0f7958
María Jesús García Godoy <mjggodoy@gmail.com>
2017-02-07 11:03:22 +0100

    The post is working but I have to fix the “empty response”

--- src/main/java/es/uma/khaos/docking_service/resource/TaskRunResource2.java
+++ src/main/java/es/uma/khaos/docking_service/resource/TaskRunResource2.java
@@ -1,5 +1,9 @@
 package es.uma.khaos.docking_service.resource;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.Random;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
@@ -9,11 +13,43 @@ import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import com.mysql.jdbc.StringUtils;
+
+import es.uma.khaos.docking_service.autodock.WorkerThread;
+import es.uma.khaos.docking_service.model.Task;
 import es.uma.khaos.docking_service.properties.Constants;
+import es.uma.khaos.docking_service.service.DatabaseService;
+import es.uma.khaos.docking_service.service.ThreadPoolService;
 
 @Path("/task3/")
 public class TaskRunResource2 extends Application {
+	
+	
+	private Response launchTask(int popSize, int evals, int runs,
+			String algorithm, int objectiveOpt)
+					throws Exception {
 
+		Random sr = SecureRandom.getInstance("SHA1PRNG");
+		String token = new BigInteger(130, sr).toString(32);
+
+		Task task = DatabaseService.getInstance().insertTask(token);
+		DatabaseService.getInstance().insertParameter(algorithm, evals, popSize, runs, objectiveOpt,
+						task.getId());
+
+		Runnable worker = new WorkerThread("DOCKING", task.getId(), algorithm,
+				runs, popSize, evals, objectiveOpt);
+		ThreadPoolService.getInstance().execute(worker);
+		
+		return Response.status(Response.Status.CREATED).build();
+
+	}
+
+	
+	private int inRangeCheck(int value, int minValue, int maxValue) {
+		if (value > maxValue) return maxValue;
+		else if (value < minValue) return minValue;
+		else return value;
+	}
+	
 	@POST
 	@Path("/post")
     @Consumes(MediaType.APPLICATION_JSON)
@@ -23,6 +59,7 @@ public class TaskRunResource2 extends Application {
 		int runs_param = Integer.parseInt(runs);
 		int population_size_param = Integer.parseInt(population_size);
 		int evaluations_param = Integer.parseInt(evaluations);
+		int objectiveOpt = 0;
 
 			try{
 				
@@ -42,6 +79,8 @@ public class TaskRunResource2 extends Application {
 								Constants.DEFAULT_MAX_NUMBER_RUNS);
 					}
 					
+				}
+				
 					
 					if (StringUtils.isNullOrEmpty(population_size)) {
 						
@@ -49,13 +88,13 @@ public class TaskRunResource2 extends Application {
 						
 					} else {
 						
-						population_size = inRangeCheck(
+						population_size_param = inRangeCheck(
 								population_size_param,
 								Constants.DEFAULT_MIN_NUMBER_POPULATION_SIZE,
 								Constants.DEFAULT_MAX_NUMBER_POPULATION_SIZE);
 					}
 					
-					if (StringUtils.isNullOrEmpty(evalsParam)) {
+					if (StringUtils.isNullOrEmpty(evaluations)) {
 						
 						evaluations_param = Constants.DEFAULT_NUMBER_EVALUATIONS;
 						
@@ -68,13 +107,15 @@ public class TaskRunResource2 extends Application {
 					}
 
 				
-				}
+				return launchTask(population_size_param, evaluations_param, runs_param, algorithm, objectiveOpt);
+
 			
 			} catch (Exception e) {
 			
 				e.printStackTrace();
 				return Response.serverError().build();
 		}
+			
 	}
 	
 }
