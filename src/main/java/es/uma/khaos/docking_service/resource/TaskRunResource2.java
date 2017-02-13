46bb5971fb6506232798b892eaa343414c95229b
María Jesús García Godoy <mjggodoy@gmail.com>
2017-02-13 13:02:37 +0100

    formatting the code…

--- src/main/java/es/uma/khaos/docking_service/resource/TaskRunResource2.java
+++ src/main/java/es/uma/khaos/docking_service/resource/TaskRunResource2.java
@@ -1,32 +1,21 @@
 package es.uma.khaos.docking_service.resource;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+
 import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.util.Random;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
-import org.glassfish.jersey.media.multipart.MultiPartFeature;
-import org.glassfish.jersey.server.ResourceConfig;
-
 import com.mysql.jdbc.StringUtils;
 import es.uma.khaos.docking_service.autodock.WorkerThread;
 import es.uma.khaos.docking_service.model.ParameterSet;
@@ -75,19 +64,6 @@ public class TaskRunResource2 extends Application {
 	        e.printStackTrace();
 	    }
 	}
-
-	
-	
-	protected void readFile(InputStream in) throws IOException{
-		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        String line;
-        while ((line = reader.readLine()) != null) {
-        	System.out.println(line);
-        }
-        
-        reader.close();	
-	}
 	
 
 	private int inRangeCheck(int value, int minValue, int maxValue) {
@@ -109,7 +85,6 @@ public class TaskRunResource2 extends Application {
 					
 		
 			String nameFile = file.getFileName();
-			System.out.println("Name file: " + nameFile);
 			unzip(nameFile);
 					
 			try{
